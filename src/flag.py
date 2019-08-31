a3bc26862c0f7bb94dc912797a12f3b511b382f0
eduardohg <eduardogiroto01@gmail.com>
2019-08-31 20:41:00 -0300

    translaçao e escala(com erro)

--- src/flag.py
+++ src/flag.py
@@ -30,3 +30,34 @@ class Flag():
         cv2.imshow('Flag', self.image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
+    def escala(self, timesX, timesY):
+        width = self.image.shape[0]
+        height = self.image.shape[1]
+
+        newImage = np.zeros((timesY*height, timesX*width, 3), np.uint8)
+        pts = np.array([[0,0], [timesY*width, 0], [timesY*width, timesX*height], [0, timesX*height]], np.int32)
+        cv2.fillPoly(newImage, [pts], (255 ,255 ,255))
+
+        for i in range(self.image.shape[0]):
+            for j in range(self.image.shape[1]):
+                pix = self.image[i][j]
+                x = i*timesY
+                y = j*timesX
+                newImage[x][y] = self.image[i][j]
+        self.image = newImage
+
+    def transl(self, x, y):
+        width = self.image.shape[0]
+        height = self.image.shape[1]
+
+        newImage = np.zeros((width, height, 3), np.uint8)
+        pts = np.array([[0,0], [width, 0], [width, height], [0, height]], np.int32)
+        cv2.fillPoly(newImage, [pts], (0, 0, 0))
+
+        for i in range(width):
+            for j in range(height):
+                pixel = self.image[i, j]
+                if((i+y) < width and (j+x) < height):
+                    newImage[i+y, j+x] = pixel
+        self.image = newImage
